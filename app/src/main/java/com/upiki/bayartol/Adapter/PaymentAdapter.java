f0fd32b8cb66c22038c56fef0ecab4cd2a47a9f0
Martino Christanto Khuangga <martino.aksel.11@gmail.com>
2017-07-01 21:36:38 +0700

    Add history page fragment without data

---
+++ app/src/main/java/com/upiki/bayartol/Adapter/PaymentAdapter.java
@@ -0,0 +1,81 @@
+package com.upiki.bayartol.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.upiki.bayartol.R;
+import com.upiki.bayartol.model.Payment;
+
+import java.util.List;
+
+/**
+ * An adapter to payment recycler view.
+ * @author Martino Christanto Khuangga <martino.aksel.11@gmail.com>
+ * @since 2017.07.01
+ */
+
+public class PaymentAdapter extends
+        RecyclerView.Adapter<
+                PaymentAdapter.PaymentViewHolder> {
+    private List<Payment> paymentList;
+
+    public class PaymentViewHolder
+            extends RecyclerView.ViewHolder {
+        public TextView tvPaymentLocation;
+        public TextView tvPaymentCost;
+        public TextView tvPaymentDate;
+        public TextView tvBusinessTrip;
+
+        public PaymentViewHolder(View itemView) {
+            super(itemView);
+            tvPaymentLocation =
+                    (TextView) itemView.findViewById(
+                            R.id.tv_payment_gate
+                    );
+            tvPaymentCost=
+                    (TextView) itemView.findViewById(
+                            R.id.tv_payment_cost
+                    );
+            tvPaymentDate =
+                    (TextView) itemView.findViewById(
+                            R.id.tv_payment_date
+                    );
+            tvBusinessTrip =
+                    (TextView) itemView.findViewById(
+                            R.id.tv_history_business_trip
+                    );
+        }
+    }
+
+    public PaymentAdapter(List<Payment> paymentList) {
+        this.paymentList = paymentList;
+    }
+
+    @Override
+    public PaymentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_payment_history, parent, false);
+        return new PaymentViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PaymentViewHolder holder, int position) {
+        Payment payment = paymentList.get(position);
+        holder.tvPaymentLocation.setText(payment.getPaymentLocation());
+        String formatedCost = String.valueOf(payment.getPaymentCost());
+        holder.tvPaymentCost.setText(formatedCost);
+        holder.tvPaymentDate.setText(payment.getPaymentDate());
+        holder.tvBusinessTrip.setVisibility(
+                payment.isBusinessTrip()
+                        ? View.VISIBLE
+                        : View.GONE);
+    }
+
+    @Override
+    public int getItemCount() {
+        return paymentList.size();
+    }
+}
