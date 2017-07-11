13076532a64647cfbf9c046691ee6b3bdfc00540
TauficLeonardoS <13514044@std.stei.itb.ac.id>
2017-07-11 11:20:33 +0800

    add row_transaction

---
+++ app/src/main/java/com/upiki/bayartol/Activity/TransactionActivity.java
@@ -0,0 +1,15 @@
+package com.upiki.bayartol.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.upiki.bayartol.R;
+
+public class TransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction);
+    }
+}
