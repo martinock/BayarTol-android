7f7f123ad53bb5c9415f73910a4049c859a16248
TauficLeonardoS <13514044@std.stei.itb.ac.id>
2017-07-15 01:53:16 +0800

    view list of member

--- app/src/main/java/com/upiki/bayartol/Activity/ListMemberActivity.java
+++ app/src/main/java/com/upiki/bayartol/Activity/ListMemberActivity.java
@@ -8,23 +8,30 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.upiki.bayartol.R;
+import com.upiki.bayartol.api.ApiClass.User;
 
 import java.util.List;
 
 public class ListMemberActivity extends AppCompatActivity {
 
     RecyclerView mListMember;
+    ProgressBar mLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_member);
 
-        mListMember = findViewById(R.id.list_member);
+        mListMember = (RecyclerView) findViewById(R.id.list_member);
+        mLoading = (ProgressBar) findViewById(R.id.loading);
+
     }
 
     public static void startThisActivity(Activity activity) {
@@ -36,9 +43,9 @@ public class ListMemberActivity extends AppCompatActivity {
         private final int MEMBER = 0;
         private final int ADD = 1;
 
-        private List<?> listMember;
+        private List<User> listMember;
 
-        public MemberAdapter(List<?> listMember) {
+        public MemberAdapter(List<User> listMember) {
             this.listMember = listMember;
         }
 
@@ -71,20 +78,48 @@ public class ListMemberActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
             if (holder instanceof MemberHolder) {
-
+                MemberHolder memberHolder = (MemberHolder) holder;
+                memberHolder.mName.setText(listMember.get(position).name);
+                memberHolder.mPhoneNumber.setText(listMember.get(position).phone_number);
             } else if (holder instanceof AddHolder) {
+                final AddHolder addHolder = (AddHolder) holder;
+                addHolder.mAddLabel.setVisibility(View.VISIBLE);
+                addHolder.mAddButton.setVisibility(View.VISIBLE);
+                addHolder.mFieldContainer.setVisibility(View.GONE);
+                addHolder.mAddContainer.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        addHolder.mAddLabel.setVisibility(View.GONE);
+                        addHolder.mAddButton.setVisibility(View.GONE);
+                        addHolder.mFieldContainer.setVisibility(View.VISIBLE);
 
+                    }
+                });
+                if (addHolder.mAddSubmit.getVisibility() == View.VISIBLE) {
+                    addHolder.mAddSubmit.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // post add new member
+                        }
+                    });
+                }
             }
         }
 
         class AddHolder extends RecyclerView.ViewHolder {
             public ImageView mAddButton;
             public TextView mAddLabel;
+            public LinearLayout mAddContainer;
+            public LinearLayout mFieldContainer;
+            public Button mAddSubmit;
 
             public AddHolder(View itemView) {
                 super(itemView);
                 mAddButton = (ImageView) itemView.findViewById(R.id.add_new_member);
                 mAddLabel = (TextView) itemView.findViewById(R.id.add_new_member_label);
+                mAddContainer = (LinearLayout) itemView.findViewById(R.id.add_container);
+                mFieldContainer = (LinearLayout) itemView.findViewById(R.id.add_field_container);
+                mAddSubmit = (Button) itemView.findViewById(R.id.add_new_member_button);
             }
         }
 
