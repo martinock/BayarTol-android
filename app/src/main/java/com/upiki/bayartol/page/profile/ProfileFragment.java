215b98b1085a9063cc7433aa40c4dc4e8096234c
TauficLeonardoS <13514044@std.stei.itb.ac.id>
2017-07-15 13:36:17 +0800

    add saveProfile()

--- app/src/main/java/com/upiki/bayartol/page/profile/ProfileFragment.java
+++ app/src/main/java/com/upiki/bayartol/page/profile/ProfileFragment.java
@@ -58,7 +58,7 @@ public class ProfileFragment extends Fragment {
         mSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                saveProfile();
             }
         });
 
