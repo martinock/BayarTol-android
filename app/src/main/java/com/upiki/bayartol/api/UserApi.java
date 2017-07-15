b2bf3f0912bf7421e384153e17e91237a34774a9
TauficLeonardoS <13514044@std.stei.itb.ac.id>
2017-07-15 12:38:48 +0800

    initialize postrequest register

--- app/src/main/java/com/upiki/bayartol/api/UserApi.java
+++ app/src/main/java/com/upiki/bayartol/api/UserApi.java
@@ -9,10 +9,16 @@ import java.util.Map;
 
 public class UserApi extends Api {
 
-    public void postUserUid(Context context, ApiListener<User> apiListener) {
+    public void postRegisterUid(Context context, String email, String password, String name, String phone_number, String address, ApiListener<User> apiListener) {
         Map<String, String> form = new HashMap<>();
-        String url = "";
-        callGetApi(context, url, form, null,User.class, apiListener);
+        form.put("role", "user");
+        form.put("email", email);
+        form.put("email", email);
+        form.put("email", email);
+        form.put("email", email);
+        String url = "http://us-central1-upiki-77460.cloudfunctions.net/register";
+
+        callGetApi(context, url, form, null, User.class, apiListener);
     }
 
     public class UserDao extends Api<User> {}
